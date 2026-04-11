12faca5a86d379c28a585e87123a16ea83301fac
radwan <www.radwaniq@gmail.com>
2026-04-11 16:50:00 +0300

    fixing syntax errors

--- src/main/java/com/ohs/project/uni/config/CorsConfig.java
+++ src/main/java/com/ohs/project/uni/config/CorsConfig.java
@@ -10,7 +10,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 public class CorsConfig implements WebMvcConfigurer {
 
-    @Value("frontend_uri")
+    @Value("${frontend_uri}")
     private String frontend_uri;
     @Override
     public void addCorsMappings(CorsRegistry registry) {
